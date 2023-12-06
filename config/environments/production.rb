0d39670aca7d2c89be7967427d8e43939e844ab4
Craig Ambrose <craig@craigambrose.com>
2023-12-06 20:38:10 +1100

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "panga_portal_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'panga.network' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
