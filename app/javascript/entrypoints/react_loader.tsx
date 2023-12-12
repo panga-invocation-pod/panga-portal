5fdc6eb680468273743f4661d78e90373428efca
Craig Ambrose <craig@craigambrose.com>
2023-12-12 18:16:24 +1100

    multiple react component roots

---
+++ app/javascript/entrypoints/react_loader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import mount from "../mount";
+import Invitation from "../root_components/Invitation";
+
+mount({
+  Invitation,
+});
---
+++ app/javascript/root_components/Invitation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Invitation = () => <h1>Hello from React!</h1>;
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+
+// root.render(<Invitation />);
+
+export default Invitation;
