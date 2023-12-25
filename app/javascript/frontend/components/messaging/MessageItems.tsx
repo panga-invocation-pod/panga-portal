55523b0181cc7567b796f80a52f11e65c35d1441
Craig Ambrose <craig@craigambrose.com>
2023-12-25 14:19:32 +1100

    wait for previous message to be printed

--- app/javascript/frontend/components/messaging/MessageItems.tsx
+++ app/javascript/frontend/components/messaging/MessageItems.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { IPrompt } from "./types"
 import MessageItem from "./MessageItem"
 
@@ -14,19 +14,28 @@ export default function MessageItems({
   onFinished,
 }: MessageItemsProps) {
   const prompts = Array.isArray(prompt) ? prompt : [prompt]
-
   if (prompts.length === 0) return null
+
+  const [index, setIndex] = useState(0)
 
   const initialCharacter = prompts[0].character
 
+  const onItemFinished = () => {
+    if (index < prompts.length - 1) {
+      setTimeout(() => setIndex(index + 1), 500)
+    } else {
+      onFinished()
+    }
+  }
+
   return (
     <div className="message-items">
-      {prompts.map((prompt, index) => (
+      {prompts.slice(0, index + 1).map((prompt, index) => (
         <MessageItem
           prompt={prompt}
           mode={mode}
           key={index}
-          onFinished={onFinished}
+          onFinished={onItemFinished}
           avatarSide={prompt.character === initialCharacter ? "left" : "right"}
         />
       ))}
