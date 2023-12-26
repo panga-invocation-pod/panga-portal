7aecdf42cb2857ffd190ce78ceb64783989ab39a
Craig Ambrose <craig@craigambrose.com>
2023-12-26 15:30:33 +1100

    responder parses if

--- lib/messaging/responders/select_option.rb
+++ lib/messaging/responders/select_option.rb
@@ -2,9 +2,28 @@ require_relative 'base_responder'
 
 module Messaging
   module Responders
+    class Option
+      def self.from_data(data)
+        hash = data.is_a?(Hash) ? data : { 'text' => data }
+
+        new text: hash['text'], if_condition: hash['if']
+      end
+
+      def initialize(text:, if_condition: nil)
+        @text = text
+        @if = if_condition
+      end
+
+      attr_reader :text, :if_condition
+
+      def as_json
+        text
+      end
+    end
+
     class SelectOption < BaseResponder
       def self.from_data(data)
-        new options: data['options']
+        new options: data['options'].map { |option| Option.from_data(option) }
       end
 
       def initialize(options:)
@@ -20,7 +39,7 @@ module Messaging
       def as_json
         {
           responder_type: responder_type,
-          options: options,
+          options: options&.map { |option| option.as_json },
         }
       end
     end
