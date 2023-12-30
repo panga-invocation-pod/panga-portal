c9e238c73ea660cac0e9fc2e7a5a6e1ddfff6ea9
Craig Ambrose <craig@craigambrose.com>
2023-12-30 14:44:38 +1100

    adding not_for_me loop

--- app/javascript/frontend/components/messaging/responders/FormResponder.tsx
+++ app/javascript/frontend/components/messaging/responders/FormResponder.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Stack } from "@chakra-ui/react"
-import { IFormResponder, Respond } from "../types"
+import { IFormResponder, IFormResponderButton, Respond } from "../types"
 import FormResponderButton from "./atoms/FormResponderButton"
 import FormResponderField from "./atoms/FormResponderField"
 
@@ -15,18 +15,32 @@ export default function FormResponder({
 }: FormResponderProps) {
   const { fields, buttons } = responder
 
+  const handleButtonClick =
+    ({ button_type, name }: IFormResponderButton) =>
+    () => {
+      if (button_type == "submit") {
+        console.log("handle submit")
+      } else {
+        respond({ text: name || button_type })
+      }
+    }
+
   return (
     <Stack spacing={4} align="stretch">
       <form>
         <Stack spacing={4} align="stretch">
           {fields.map((field) => (
-            <FormResponderField field={field} errors={null} />
+            <FormResponderField key={field.name} field={field} errors={null} />
           ))}
         </Stack>
       </form>
       <div className="button-group">
         {buttons.map((button) => (
-          <FormResponderButton {...button} />
+          <FormResponderButton
+            {...button}
+            key={button.name}
+            onClick={handleButtonClick(button)}
+          />
         ))}
       </div>
     </Stack>
--- app/javascript/frontend/components/messaging/responders/atoms/FormResponderButton.tsx
+++ app/javascript/frontend/components/messaging/responders/atoms/FormResponderButton.tsx
@@ -3,16 +3,22 @@ import { IFormResponderButton } from "../../types"
 import { Button } from "@chakra-ui/react"
 import { nameToTitle } from "../../../utility/strings"
 
+type FormResponderButtonProps = IFormResponderButton & {
+  onClick?: () => void
+}
+
 const FormResponderButton = ({
   button_type,
   name,
   text,
-}: IFormResponderButton) => {
+  onClick,
+}: FormResponderButtonProps) => {
   return (
     <Button
       type="submit"
       colorScheme="primary"
       variant={button_type == "submit" ? "solid" : "outline"}
+      onClick={onClick}
     >
       {text || nameToTitle(name)}
     </Button>
