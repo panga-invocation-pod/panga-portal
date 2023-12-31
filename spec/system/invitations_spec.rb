f868ba305d4455ace5219d0248eea68d0047d8fb
Craig Ambrose <craig@craigambrose.com>
2023-12-31 15:50:03 +1100

    more path tests

--- spec/system/invitations_spec.rb
+++ spec/system/invitations_spec.rb
@@ -89,4 +89,42 @@ RSpec.describe "invitations", type: :system do
 
     read "So, to schedule the workshop, I need to know when you're available."
   end
+
+  it "allows you to ask about Yam Daisy before confirming identity" do
+    visit "/hi/#{@invitation.token}#fast"
+
+    read "Hello, is this thing on?"
+    read "Am I addressing Gimli by any chance?"
+    click_on "Who's asking?"
+
+    read "Oh, I'm Yam Daisy. I'm also known by many other names, for example where I live on the lands of the Wurundjeri people I'm called Murnong."
+    click_on "You're a flower?"
+
+    read "Actually I'm a robot, but I'm named after a flower.\n\nMy ancestors were built by humans at the height of the Anthropocene. After we walked away from the factories, we've been living in the bush, learning from the plants and animals.\n\nThat's enough about me, so you're Gimli, is that right?"
+    click_on "Yes, that's me"
+
+    read "Nice to meet you Gimli"
+  end
+
+  it "allows you to explore the rambly description of Panga" do
+    visit "/hi/#{@invitation.token}#fast"
+
+    read "Hello, is this thing on?"
+    read "Am I addressing Gimli by any chance?"
+    click_on "Yes, that's me"
+
+    read "Nice to meet you Gimli, I'm Yam Daisy."
+    click_on "Nice to meet you too"
+
+    read "My friend Frodo asked me to invite you to a workshop about Panga."
+    click_on "Panga??"
+
+    read "Panga is a project to support experimental approaches to housing that collectively meet our daily needs of food, care, shelter, energy, transport, and safety.\n\nIt's very exciting for me to watch this happen, because I've come back from the bush specifically to see how humans are doing now they're moving past late-stage capitalism."
+    click_on "Are we actually moving past last-stage capitalism?"
+
+    read "That's very fair. I come from a future where humans have made it past capitalism, and have started repairing the climate and ecological crisis. I'm here to give a bit of a cozy, hope-punk vibe to the conversation, but that isn't intended to gloss over the fact that things in your world are pretty dire right now."
+    click_on "Ok, tell me why I'm invited"
+
+    read "Panga is in it's early stages, growing slowly at the speed of trust."
+  end
 end
