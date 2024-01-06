ecf19db55a594e7f7402720eab587b89e620d26d
Craig Ambrose <craig@craigambrose.com>
2024-01-06 19:34:35 +1100

    add yam daisy invite mailer

--- app/mailers/yam_daisy_mailer.rb
+++ app/mailers/yam_daisy_mailer.rb
@@ -4,4 +4,9 @@ class YamDaisyMailer < ApplicationMailer
   def test_email
     mail(to: params[:to], subject: 'Test email')
   end
+
+  def first_workshop_invite
+    @invitation = Invitation.find(params[:invitation_id])
+    mail(to: @invitation.invitee_email, subject: 'Your Panga Workshop Invitation')
+  end
 end
