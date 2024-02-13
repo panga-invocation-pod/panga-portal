f8770b6d743d711e87771c5e5c59f131ba5342b1
Craig Ambrose <craig@craigambrose.com>
2024-02-13 16:21:45 +1100

    can create location

--- app/controllers/admin/workshop_locations_controller.rb
+++ app/controllers/admin/workshop_locations_controller.rb
@@ -3,7 +3,7 @@ class Admin::WorkshopLocationsController < AdminController
 
   private
 
-  # def permit_record_fields(fields)
-  #   fields.permit()
-  # end
+  def permit_record_fields(fields)
+    fields.permit(:name, :directions, :accessibility, :address_name, :address_street, :address_city, :address_state, :address_postcode, :address_traditional_country)
+  end
 end
--- app/models/workshop_location.rb
+++ app/models/workshop_location.rb
@@ -1,2 +1,4 @@
 class WorkshopLocation < ApplicationRecord
+  validates :name, presence: true
+  validates :address_street, :address_city, :address_state, :address_traditional_country, presence: true
 end
