76c9120b2df8ca42e759eede95bf9709f52e1e5c
Craig Ambrose <craig@craigambrose.com>
2024-03-14 21:05:47 +1100

    fix email url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "panga_portal_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'panga.network' }
+  config.action_mailer.default_url_options = { host: 'portal.panga.network' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV.fetch("SMTP_HOST", "mail.panga.network"),
